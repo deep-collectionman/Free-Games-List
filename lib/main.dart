e22f7a1ebfe96d224f3c708a34695d6d906dfcaf
deep-collectionman <jmcollectionman@gmail.com>
2022-05-29 11:46:12 -0300

    Moving MultiBlocProvider to HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:free_games/models/bloc/free_games_bloc.dart';
-import 'package:free_games/models/genres.dart';
 
 import 'package:free_games/screens/home_screen.dart';
 
@@ -15,19 +12,13 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => MostRecentBloc()..add(const FreeGamesFetchMostRecentEvent())),
-        BlocProvider(create: (context) => FreeGamesBloc()..add(const FreeGamesFetchByCategoryEvent(genre: Genre.moba))),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Free Games',
-        theme: ThemeData(
-          scaffoldBackgroundColor: const Color(0xFF2A2E33),
-        ),
-        home: const HomeScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Free Games',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF2A2E33),
       ),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:free_games/views/game_list_section.dart';
 import 'package:free_games/views/genres_section.dart';
 import 'package:free_games/views/most_recent_section.dart';
+import 'package:free_games/models/bloc/free_games_bloc.dart';
+import 'package:free_games/models/genres.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Free Games'),
-        centerTitle: true,
-        backgroundColor: const Color(0xFF020202),
-      ),
-      body: Center(
-        child: SizedBox(
-          height: double.infinity,
-          child: ListView(
-            children: const [
-              GenresSection(),
-              MostRecentSection(),
-              GameListSection(),
-            ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => MostRecentBloc()..add(const FreeGamesFetchMostRecentEvent())),
+        BlocProvider(create: (context) => FreeGamesBloc()..add(const FreeGamesFetchByCategoryEvent(genre: Genre.moba))),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Free Games'),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF020202),
+        ),
+        body: Center(
+          child: SizedBox(
+            height: double.infinity,
+            child: ListView(
+              children: const [
+                GenresSection(),
+                MostRecentSection(),
+                GameListSection(),
+              ],
+            ),
           ),
         ),
       ),
